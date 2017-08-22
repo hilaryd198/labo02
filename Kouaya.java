0bc4e26e3a0569427053d5c0d35452cfc3d806ce
Kouaya <kouaya2@yahoo.fr>
2017-08-21 22:55:52 -0400

    J'ai ajouté un fichier source .java et modifier le fichier .md

---
+++ Kouaya.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world from GIT!");
+                System.out.println("Etat aprèsla premiere modifications!");
+                System.out.println("Change after Pushing to remote!");
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # d73-labo-git-02
+This is the readme file for HelloWorld project.
